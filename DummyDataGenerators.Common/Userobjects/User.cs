8d0e7703e20ec8d2085288d2ab8212da80eb570a
Marius Kristiansen <thewisetorsk@gmail.com>
2021-05-09 21:05:43 +0200

    Move user object to common

---
+++ DummyDataGenerators.Common/Userobjects/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyDataGenerators.Common.Userobjects
+{
+    public class User
+    {
+
+        public Visible Visibilities { get; set; }
+        public User()
+        {
+            Visibilities = new();
+        }
+
+        public class Visible
+        {
+            public bool Inputfield { get; set; } = false;
+        }
+    }
+}
